4368df8f290162d861a65cbbdc338c88138f1b5b
Sergio <alejandrosamt@gmail.com>
2023-06-21 10:40:04 -0400

    car details

--- types/index.ts
+++ types/index.ts
@@ -6,9 +6,27 @@ export interface CustomButtonProps {
     handleClick?:
     MouseEventHandler<HTMLButtonElement>;
     btnType?: 'button' | 'submit';
+    textStyles?: string;
+    rightIcon?: string;
+    isDisable?: boolean;
 }
 
-export interface SearchManufacturerProps{
+export interface SearchManufacturerProps {
     manufacturer: string;
     setmanufacturer: (manufacturer: string) => void;
+}
+
+export interface CardProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
 }
